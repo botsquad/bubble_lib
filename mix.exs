b018c8d1348c19db9bf00870c556722238b48ad2
Arjan Scherpenisse <arjan@scherpenisse.net>
2018-09-24 15:08:49 +0200

    Packaging

--- mix.exs
+++ mix.exs
@@ -6,9 +6,27 @@ defmodule BubbleLib.MixProject do
       app: :bubble_lib,
       version: File.read!("VERSION"),
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/botsquad/bubble_lib",
+      homepage_url: "https://github.com/botsquad/bubble_lib",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
+  end
+
+  defp description do
+    "Collection of utility functions for Botsquad's BubbleScript."
+  end
+
+  defp package do
+    %{
+      files: ["lib", "mix.exs", "*.md", "LICENSE", "VERSION"],
+      maintainers: ["Arjan Scherpenisse"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/botsquad/bubble_lib"}
+    }
   end
 
   # Run "mix help compile.app" to learn about applications.
